b2f42bd15eabaa4378964758f34845a90b1add87
Karsten Lindenburg <102625@glr.nl>
2024-10-16 14:11:16 +0000

    Refactor scroll behavior and add fade-in effect for elements

--- Karsten/Styles/JS/script.js
+++ Karsten/Styles/JS/script.js
@@ -1,6 +1,43 @@
-document.querySelector('.trailer-button').addEventListener('click', function () {
-    window.scrollTo({
-        top: document.querySelector('.hero').offsetTop,
-        behavior: 'smooth'
+const navbar = document.querySelector('.navbar');
+const heroImage = document.querySelector('.hero-background img');
+
+let lastScrollY = window.scrollY;
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            if (window.scrollY < lastScrollY) {
+                navbar.style.top = "0";
+            } else {
+                navbar.style.top = "-100px";
+            }
+        } else {
+            navbar.style.top = "0"; 
+        }
     });
+    lastScrollY = window.scrollY;
+}, {
+    threshold: 0.3 
+});
+
+if (heroImage) {
+    observer.observe(heroImage);
+}
+
+
+const faders = document.querySelectorAll('.fade-in');
+
+const fadeObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.25
+});
+
+faders.forEach(fader => {
+    fadeObserver.observe(fader);
 });
