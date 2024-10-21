5fae8540076703e40c78e5ccd876e5473059e9e3
Karsten Lindenburg <102625@glr.nl>
2024-10-21 12:00:36 +0000

    nieuwe buttons en frames geadd en beter georganizeerd

--- Karsten/Styles/JS/script.js
+++ Karsten/Styles/JS/script.js
@@ -26,11 +26,11 @@ const fadeObserver = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('visible');
-            observer.unobserve(entry.target); // Stop observing once element is visible
+            observer.unobserve(entry.target);
         }
     });
 }, {
-    threshold: 0.25
+    threshold: 0.4
 });
 
 faders.forEach(fader => {
